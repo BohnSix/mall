b5241bb50261c857b136c710ba17eeff9ab6c273
bohnsix <bohn_six@163.com>
2021-09-17 10:45:39 +0800

    hello, world

---
+++ src/main/java/com/bohnsix/mall/HelloController.java
@@ -0,0 +1,13 @@
+package com.bohnsix.mall;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello, world";
+    }
+}
